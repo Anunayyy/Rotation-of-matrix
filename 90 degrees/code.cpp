79a11cc0a7d629c1a8a4c545887ae710416d734a
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-17 00:48:04 +0530

    Create code.cpp

---
+++ 90 degrees/code.cpp
@@ -0,0 +1,21 @@
+// first we make a tranpose and then we swap the columns to make a 90 degree.
+
+void rotate(vector<vector<int>>& matrix) {
+		// we first make the transpose of the matrix
+        int n = matrix.size();
+        for(int i=0; i<n; ++i) {
+            for(int j=i; j<n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; ++i) {
+		// we then using the 2 pointer approach swap the columns. it is really easy to imagine of you use a 3x3 matrix in your mind. even a 4x4 is easy.
+            int left = 0, right = n-1;
+            while(left < right) {
+                swap(matrix[i][left], matrix[i][right]);
+                ++left;
+                --right;
+            }
+        }
+    }
